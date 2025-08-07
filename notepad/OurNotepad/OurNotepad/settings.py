f41a13cc1b4829c53a24c638863486ccc3f69dc5
Your Name <chrisscolin13@gmail.com>
2025-08-07 16:25:57 +0200

    removed live reload

--- notepad/OurNotepad/OurNotepad/settings.py
+++ notepad/OurNotepad/OurNotepad/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Main',
-    'livereload',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'OurNotepad.urls'
